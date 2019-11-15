867c7b6a6966aedc60c3a80cb8753f78825b7159
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2019-11-15 23:08:16 +0100

    add tests

--- dump.py
+++ dump.py
@@ -1,10 +1,15 @@
-import json
-
 import requests
+from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
 
+import helpling_schema
 from query_strings import fetch_candidates, transition_to_provider_selection
 
 BASE_URL = "https://www.helpling.de/api/"
+gql_endpoint = HTTPEndpoint(BASE_URL + "v2/rr", base_headers={
+    # CloudFlare blocks the default user agent. Pretend to be IE 6.
+    "User-Agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)"
+})
 
 
 class TransitionBidToProviderSelection:
@@ -33,7 +38,10 @@ def create_bid(postcode: int, **kwargs) -> str:
     response = requests.post(BASE_URL + "v1/bids", {"bid[postcode]": postcode, "bid[checkout_version]": 1})
     bid_id = response.json().get("data").get("code")
 
-    print("Create bid for " + str(postcode) + " (" + bid_id + "): " + str(response.status_code))
+    if bid_id is None:
+        raise Exception("Bid for postcode " + str(postcode) + " could not be created: " + response.text)
+
+    print("Create bid for " + str(postcode) + " (" + bid_id + "): OK")
 
     variable_defaults = TransitionBidToProviderSelection().__dict__
     response = requests.post(BASE_URL + "/v2/rr", json={
@@ -42,9 +50,10 @@ def create_bid(postcode: int, **kwargs) -> str:
         "variables": {**variable_defaults, **kwargs, "bidCode": bid_id}
     })
 
-    transition_is_success = response.json().get("errors") is None
-    print("Parametrize bid " + bid_id + ": " + str(response.status_code) if transition_is_success else response.text)
+    if response.json().get("errors") is not None:
+        raise Exception("Bid " + bid_id + " could not be parametrized: " + response.text)
 
+    print("Parametrize bid " + bid_id + ": OK")
     return bid_id
 
 
@@ -66,6 +75,19 @@ def get_candidates(bid_id: str):
     return response.json().get("data").get("customerBid").get("potentialCandidates")
 
 
+def get_bid(bid_id: str) -> helpling_schema.CustomerBid:
+    op = Operation(helpling_schema.Query)
+
+    bid = op.customer_bid(code=bid_id)
+    bid.code()
+    bid.duration()
+    bid.start_time()
+
+    data = gql_endpoint(op)
+    return (op + data).customer_bid
+
+
 if __name__ == "__main__":
-    bid_id = create_bid(10179, time="11:30")
-    print(json.dumps(get_candidates(bid_id), indent=4))
+    new_bid = create_bid(10179, time="11:30")
+    get_bid(new_bid)
+    # print(json.dumps(get_candidates(bid), indent=4))
---
+++ test/test-dump.py
@@ -0,0 +1,39 @@
+import datetime
+import unittest
+
+from dateutil.parser import parse as parse_date
+from ddt import ddt, data, unpack
+
+import dump
+
+
+# noinspection PyBroadException
+@ddt
+class TestCreateBid(unittest.TestCase):
+    @data(69190, 76137)
+    def test_bid_exists_after_create(self, postcode):
+        bid_id = dump.create_bid(postcode)
+        self.assertEqual(bid_id, dump.get_bid(bid_id).code)
+
+    @data(12345)
+    def test_bid_does_not_exist_for_invalid_postcode(self, postcode):
+        bid_id = None
+        try:
+            bid_id = dump.create_bid(postcode)
+        except:
+            pass
+        finally:
+            self.assertTrue(bid_id is None)
+
+    def test_bid_has_default_values(self):
+        bid_id = dump.create_bid(10179)
+        created_bid = dump.get_bid(bid_id)
+        self.assertEqual(12600, created_bid.duration)
+
+    @unpack
+    @data(["17:00", "26/05/2020"], ["10:15", "01/04/2020"])
+    def test_bid_has_custom_values(self, time, date):
+        bid_id = dump.create_bid(76137, time=time, date=date)
+        created_bid = dump.get_bid(bid_id)
+        expected = parse_date(time + " " + date, dayfirst=True).astimezone(datetime.timezone.utc)
+        self.assertEqual(expected, created_bid.start_time)
